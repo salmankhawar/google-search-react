52806388a7096b5d68f2e3618d5920bcec46acf4
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-04-18 13:40:43 +0700

    Added Data Array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import logo from './logo.svg'
 import './App.css'
 
+// Data
+let data = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+]
+
 function App() {
   return (
     <>
