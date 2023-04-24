8fd110aa63767460a1ab27277b4cb6d5604128c5
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-04-24 16:29:01 +0700

    Added Search Function - Defined Results

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import logo from './logo.svg'
 import './App.css'
 import Results from './Results'
-
-// Data
-let data = [
-  {
-    title: 'JS tutorials',
-    description: 'The best JavaScript tutorials in the galaxy!',
-    url: 'https://www.w3schools.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js',
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js',
-      },
-    ],
-  },
-]
+import { useState } from 'react'
 
 function App() {
+  // States
+
+  const [results, setResults] = useState([])
+
+  // Data
+
+  let data = [
+    {
+      title: 'JS tutorials',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'https://www.w3schools.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js',
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js',
+        },
+      ],
+    },
+  ]
+
+  function search(string) {
+    let filteredResults = data.filter(
+      (n) =>
+        n.url.toLowerCase().includes(string.toLowerCase()) ||
+        n.description.toLowerCase().includes(string.toLowerCase()) ||
+        n.title.toLowerCase().includes(string.toLowerCase())
+    )
+    setResults(filteredResults)
+  }
+
   return (
     <>
       <div className="header">
@@ -31,7 +47,7 @@ function App() {
           <button>Search</button>
         </form>
       </div>
-      <Results data={data} />
+      <Results results={results} />
     </>
   )
 }
--- src/Results.js
+++ src/Results.js
@@ -4,10 +4,10 @@ export default function Results(props) {
   return (
     <div className="main">
       <span className="nresults">
-        {props.data.length} Result{props.data.length != 1 ? 's' : ''}
+        {props.results.length} Result{props.results.length != 1 ? 's' : ''}
       </span>
 
-      {props.data.map((element, i) => (
+      {props.results.map((element, i) => (
         <Result element={element} key={i} />
       ))}
     </div>
